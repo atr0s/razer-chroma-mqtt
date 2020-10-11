6dfd262544980da74c22a8cc292ae3424d8bee1b
root <root@pyke.localdomain>
2020-10-11 16:06:11 +1300

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# Razer Chroma MQTT 
+
+Turn your Razer Chroma devices into an MQTT light entity in home assistant.
+
+# Features
+  - Allows you to control the colours of all your Chromalink devices through the Razer Chroma API, I found that some devices don't seem to work via the REST API, couldn't be bothered to figure out why 
+  - Saves the last state and restores it upon startup
+
+# How to get it going
+
+On the machine where Razer Chroma is installed
+  - Install nodejs
+  - Install requirements 
+ ``` npm install ```
+  - Create config.json containing your MQTT endpoint and configured device in Home Assistant
+  ```json
+      {
+        "url": "mqtt://<mqtt endpoint>",
+        "client_id": "<client identifier>"
+      }
+  ```
+  - Run it ```node razer-mqtt.js```
+
+On home assistant:
+  - Add an entity on the configuration as per below, not that the topic name must match the name you've picked on the last step.
+```
+    light:
+      - platform: mqtt
+        name: "Razer Chroma"
+        schema: json
+        command_topic: "<client identifier>/set"
+        state_topic: "<client identifier>/state"
+        rgb: true
+        brightness: false
+```
+
+You can use something like NSSM to run the code a service that starts up with your computer [NSSM](https://nssm.cc/)
+
+# Todo
+  - Support effects
+  - Write tests
---
+++ razer-mqtt.js
@@ -0,0 +1,71 @@
+const mqtt = require('mqtt');
+const store = require('data-store')({ path: process.cwd() + '/state.json' });
+
+const config = require(process.cwd() + '/config.json');
+const Chroma = require("razer-chroma-nodejs");
+let devicesColor;
+const url = config.url;
+const clientId = config.client_id;
+const options = {
+  clientId: clientId
+}
+
+var client = mqtt.connect(url,options)
+
+function closeConnection(){
+   Chroma.util.uninit(() => {
+        console.log("Chroma Editing Stopped");
+        client.end();
+    });
+  }
+
+  client.on("connect",function(){	
+    console.log(`Connected to ${url}`);
+    Chroma.util.init(() => {
+        console.log("Chroma Editing Started");
+        setTimeout(() => {
+          let previousState = JSON.stringify(store.data);
+          if(previousState) {
+            console.log("Restoring previous state");
+            updateChroma('state_restore',previousState,'');
+          }
+        }, 2000);
+    });
+})
+
+function updateChroma(topic, message, packet){
+    const payload = JSON.parse(message);
+
+    if(payload.state === 'ON') {
+        if(devicesColor) {
+          Chroma.effects.all.setColor(devicesColor);
+        } else {
+          Chroma.effects.all.setColor(store.get('colors'));
+        }
+    }
+
+    if (payload.color) {
+        const c = payload.color;
+        devicesColor = Chroma.colors.rgb(c.r,c.g,c.b);
+        store.set('color',payload.color);
+        Chroma.effects.all.setColor(devicesColor);
+    }
+
+    if(payload.state === 'OFF') {
+        Chroma.effects.all.off();
+    }
+
+    if(payload.state) {
+        store.set('state',payload.state);
+    }
+    client.publish(`${clientId}/state`,JSON.stringify(store.data));
+
+    
+}
+
+client.subscribe([`${clientId}/set`]);
+client.on('message',updateChroma);
+
+process.on('SIGKILL', closeConnection);
+process.on('SIGINT', closeConnection);
+
